0ff73ea8506e04ab3b5b57a4dcc422a874d7dd36
velmouth-star <shanpuyangshu@gmail.com>
2022-09-15 19:11:53 +0900

    routes記述

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('planet', \App\Http\Controllers\PlanetController::class);
