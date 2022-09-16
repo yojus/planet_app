07855ba8fcd2aa5080b739d72e246ef88a3bf6a5
velmouth-star <shanpuyangshu@gmail.com>
2022-09-16 14:06:08 +0900

    error css適用

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
@@ -28,19 +29,19 @@
         @csrf
         <p>
             <label for="name">名前</label>
-            <input type="text" name="name" id="name" value="{{ old('name') }}">
+            <input type="text" name="name" value="{{ old('name') }}">
         </p>
         <p>
             <label for="en_name">名前(英語)</label>
-            <input type="text" name="en_name" id="en_name" value="{{ old('en_name') }}">
+            <input type="text" name="en_name" value="{{ old('en_name') }}">
         </p>
         <p>
             <label for="radius">半径</label>
-            <input type="number" name="radius" id="radius" value="{{ old('radius') }}">
+            <input type="number" name="radius" value="{{ old('radius') }}">
         </p>
         <p>
             <label for="weight">重量</label>
-            <input type="number" name="weight" id="weight" value="{{ old('weight') }}">
+            <input type="number" name="weight" value="{{ old('weight') }}">
         </p>
 
         <input type="submit" value="登録">
